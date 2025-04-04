5d27df38bd5ffeaf6537a6ac9e5bd85a030b076e
Megakuul <linus.moser@megakuul.ch>
2025-04-04 10:32:30 +0200

    chore: update nvchad mappings

--- config/nvim/lua/configs/lspconfig.lua
+++ config/nvim/lua/configs/lspconfig.lua
@@ -3,7 +3,7 @@ local lspconfig = require "lspconfig"
 
 nvlsp.defaults() -- loads nvchad's defaults
 
-local servers = { "html", "cssls", "gopls", "clangd", "svelte" }
+local servers = { "html", "cssls", "gopls", "clangd", "svelte", "tailwindcss" }
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
