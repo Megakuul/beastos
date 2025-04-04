cc58dbde695b0a182e815b04ba880c42ba9662c3
Megakuul <linus.moser@megakuul.ch>
2025-04-04 11:38:04 +0200

    feat: add nvim oil

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -16,9 +16,27 @@ return {
     "nvim-treesitter/nvim-treesitter",
     opts = {
       ensure_installed = {
-        "vim", "lua", "vimdoc",
-       "html", "css", "go", "c", "cpp", "svelte", "proto"
+        "vim", "lua", "vimdoc", "toml", "yaml", "json", 
+       "html", "css", "javascript", "go", "c", "cpp", "svelte", "proto"
       },
     },
   },
+
+  {
+    "stevearc/oil.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    lazy = false,
+    config = function()
+      require("oil").setup {
+        columns = {"icon"},
+        keymaps = {
+          ["<C-h>"] = false,
+        },
+        view_options = {
+          show_hidden = true,
+        },
+      }
+    vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+    end,
+  }
 }
