e249501a3f3157bbd88037db57b7df3e0d95cf84
Megakuul <linus.moser@megakuul.ch>
2026-05-09 22:30:31 +0200

    feat: add treesitter expanding feature with ö

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -116,3 +116,21 @@ vim.keymap.set("n", "gri", ":Telescope lsp_implementations<CR>")
 vim.keymap.set("n", "<leader>tb", ":Gitsigns toggle_current_line_blame<CR>")
 
 require "sound"
+
+vim.keymap.set("n", "ö", "v")
+
+vim.keymap.set({ "x", "o" }, "ö", function()
+  if vim.treesitter.get_parser(nil, nil, { error = false }) then
+    require("vim.treesitter._select").select_parent(vim.v.count1)
+  else
+    vim.lsp.buf.selection_range(vim.v.count1)
+  end
+end, { desc = "Select parent treesitter node or outer incremental lsp selections" })
+
+vim.keymap.set({ "x", "o" }, "é", function()
+  if vim.treesitter.get_parser(nil, nil, { error = false }) then
+    require("vim.treesitter._select").select_child(vim.v.count1)
+  else
+    vim.lsp.buf.selection_range(-vim.v.count1)
+  end
+end, { desc = "Select child treesitter node or inner incremental lsp selections" })
